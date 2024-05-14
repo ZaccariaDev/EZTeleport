25a21fadff1cdd92eca4d71819f4512a12ff10ab
TheGameBoy95 - Zaccaria <haroual.zaccaria@gmail.com>
2024-05-14 05:47:53 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,10 +3,15 @@ game 'gta5'
 
 author 'TheGameBoy_95 - Assistance https://discord.gg/jUS8cjsH68'
 description 'EZTeleports - Permet aux administrateurs de créer des points de téléportation personnalisés sur leur serveur. Facile à configurer et à utiliser. | EZTeleports - Allows administrators to create custom teleportation points on their server. Easy to set up and use.'
-version '1.0.0'
+version '1.1.0'
 
-shared_script 'Config.lua'
-client_script 'client/cl_main.lua'
-server_script 'server/sv_main.lua'
+client_scripts {
+    'client/cl_main.lua',
+}
 
-
+server_scripts {
+    'lang/locales.lua',
+    'server/Config.lua',
+    'server/sv_main.lua',
+    'server/sv_version_check.lua'
+}
