1ab70b92c9fb012aaafb3cd99b333ca95808409c
TheGameBoy95 - Zaccaria <haroual.zaccaria@gmail.com>
2024-05-14 05:38:56 +0200

    Update sv_version_check.lua

--- server/sv_version_check.lua
+++ server/sv_version_check.lua
@@ -1,11 +1,25 @@
-local versionUrl = 'https://raw.githubusercontent.com/ZaccariaDev/EZTeleport/main/VERSION'
+local versionUrl = "https://raw.githubusercontent.com/ZaccariaDev/EZTeleport/main/VERSION"
 
-CreateThread(function()
-    Wait(5000) 
+local function checkVersion()
     local currentVersion = Config.Version
-    local versionCheck = PerformHttpRequest(versionUrl, function(err, response, headers)
-        if currentVersion ~= response then
-            print('^1[EZTeleports] Une nouvelle version est disponible: ' .. response .. '. Vous utilisez la version ' .. currentVersion .. '.^7')
+    print('^3[EZTeleports] Vérification de la version...^7')
+    PerformHttpRequest(versionUrl, function(err, response, headers)
+        if response then
+            local onlineVersion = response:gsub("%s+", "") -- Suppression des espaces et des retours à la ligne
+            if currentVersion ~= onlineVersion then
+                print('^1[EZTeleports] Une nouvelle version est disponible: ' .. onlineVersion .. '. Vous utilisez la version ' .. currentVersion .. '.^7')
+            else
+                print('^2[EZTeleports] Vous utilisez la dernière version: ' .. currentVersion .. '.^7')
+            end
+        else
+            print('^1[EZTeleports] Erreur lors de la vérification de la version.^7')
         end
     end, 'GET')
+end
+
+-- Vérification de la version au démarrage du mod
+AddEventHandler('onResourceStart', function(resourceName)
+    if GetCurrentResourceName() == resourceName then
+        checkVersion()
+    end
 end)
