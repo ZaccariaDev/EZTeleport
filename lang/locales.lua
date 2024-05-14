7eff0b44f3dfe14b6b6daedb5f10851f25d9a193
TheGameBoy95 - Zaccaria <haroual.zaccaria@gmail.com>
2024-05-14 05:45:28 +0200

    Add files via upload

---
+++ lang/locales.lua
@@ -0,0 +1,36 @@
+Locales = {}
+
+Locales['fr'] = {
+    ['no_access'] = "Vous ne remplissez pas les conditions pour vous téléporter.",
+    ['teleport_success'] = "Téléportation réussie.",
+    ['discord_log_title'] = "Téléportation",
+    ['discord_log_message'] = "%s s'est téléporté à %s.",
+    ['discord_log_footer'] = "EZTeleport Logs",
+    ['press_e_to_teleport'] = "[E] pour se téléporter",
+    ['teleport_hopital_marker'] = "Aller à l'hopital",
+    ['teleport_hopital_destination'] = "Sortir de l'hopital",
+    ['teleport_hopital_message'] = "Vous vous êtes téléporté à l'hopital.",
+    ['teleport_hopital_return_message'] = "Vous êtes retourné à l'emplacement précédent depuis l'hopital.",
+    ['error_missing_fields'] = "Erreur de configuration pour le point de téléportation : %s. Le nom, la position de téléportation (tpPos), et la destination sont obligatoires.",
+    ['error_teleport_not_found'] = "Erreur: Point de téléportation non trouvé pour %s",
+    ['teleport_Comissariat_marker'] = "Aller au Comissariat",
+    -- Ajoutez d'autres points de téléportation ici
+}
+
+Locales['en'] = {
+    ['no_access'] = "You do not meet the conditions to teleport.",
+    ['teleport_success'] = "Teleportation successful.",
+    ['discord_log_title'] = "Teleportation",
+    ['discord_log_message'] = "%s teleported to %s.",
+    ['discord_log_footer'] = "EZTeleport Logs",
+    ['press_e_to_teleport'] = "[E] to teleport",
+    ['teleport_hopital_marker'] = "Go to the hospital",
+    ['teleport_hopital_destination'] = "Exit the hospital",
+    ['teleport_hopital_message'] = "You teleported to the hospital.",
+    ['teleport_hopital_return_message'] = "You returned to the previous location from the hospital.",
+    ['error_missing_fields'] = "Configuration error for teleport point: %s. Name, teleport position (tpPos), and destination are required.",
+    ['error_teleport_not_found'] = "Error: Teleport point not found for %s",
+    ['teleport_Comissariat_marker'] = "Go to police station",
+
+    -- Add new teleportation points here if you've set language to 'en'
+}
